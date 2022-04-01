551f56a2e659673220160d14cbf4053c19554010
Matthew Collier <mcollier@digitalbazaar.com>
2022-04-01 11:57:18 -0400

    Make `MEDIATOR` configurable.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # chapi-demo-issuer Changelog
 
+## 1.1.0 - TBD
+
+### Changed
+- Make `MEDIATOR` configurable.
+
 ## 1.0.0 - 2022-04-01
 
 - See individual commits for history.
---
+++ config.js
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict'
+
+/**
+ * Simple config file (to help test against local instances of authn.io, local
+ * wallets, etc).
+ */
+
+const MEDIATOR = 'https://authn.io/mediator' + '?origin=' +
+  encodeURIComponent(window.location.origin);
